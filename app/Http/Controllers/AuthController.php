6ab62cf3bdc4cbb44bd28e14ede29f558a16ee98
Jayank-Tiwari <106905122+Jayank-Tiwari@users.noreply.github.com>
2025-08-27 18:26:47 +0530

    Enhance password validation in AuthController
    Updated password validation rules to enforce minimum length and complexity requirements.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,7 +22,15 @@ class AuthController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => [
+    'required',
+    'confirmed',
+    Password::min(8) // minimum length
+        ->mixedCase() // requires uppercase + lowercase
+        ->letters()   // requires at least one letter
+        ->numbers()   // requires at least one number
+        ->symbols()   // requires at least one special character
+],
         ]);
 
         $user = User::create([
